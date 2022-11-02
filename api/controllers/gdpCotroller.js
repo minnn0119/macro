e9091719ac613fae3dbf57cbd525014eac59a52a
minnn0119 <83383771+minnn0119@users.noreply.github.com>
2022-11-02 15:49:38 +0700

    Update api
    Minh add

--- api/controllers/gdpCotroller.js
+++ api/controllers/gdpCotroller.js
@@ -9,11 +9,15 @@ module.exports = {
 
     //get all data gdp
     get: (req, res)=> {
+        res.json({data : sData})
+    },
+    detail: (req,res)=>{
+        
         let i = req.query.id ;
         //ii là số tháng vd: 3M->i=3, 1U-> i=12, all->1200
         let ii = req.query.time;
         const time =new Date()
-        let aa = req.query.type
+        let aa = req.params.type
         new Date (time.setMonth(time.getMonth()-ii))
         if(aa=='gdp'){
             const sData = db.GDP
@@ -44,7 +48,7 @@ module.exports = {
             }
         }
         else{
-                res.json({data : sData}) 
+            res.send('Nothing')
         }
     },
     //post data lên bảng tùy chỉnh đồ thị
@@ -63,23 +67,42 @@ module.exports = {
         dataPost.splice(index,1);
         res.send(dataPost)
     },
-
     search: (req,res) => {
-        // var id = req.p.id;
-	    // var data1 = posts.filter(function(item){
-        //     return item.id === parseInt(id)
-        // });
-        var id = req.query.id;
-        var type = req.query.type;
-        var data = sData.filter(items => items.type === type ||items.name === id ) ;
-        res.send(data) 
+        let aa = req.params.type
+        console.log(req.query)
+        if(aa=='gdp'){
+            const sData = db.GDP
+            var id = req.query.id;
+            var type = req.query.type;
+            var data = sData.filter(items => items.type === type ||items.name === id) ;
+            res.send(data) 
+        }
+        if(aa=='cpi'){
+            const sData = db.CPI
+            var id = req.query.id;
+            var type = req.query.type;
+            var data = sData.filter(items => items.type === type ||items.period === id ) ;
+            res.send(data) 
+        }
     },
     paging: (req,res) => {
-        let perPage = 10; 
-        let page = parseInt(req.params) || 1; 
-        var start =(page-1)*perPage
-        var end = page *perPage
-       res.send(sData.slice(start,end)) 
+        let aa = req.params.type
+        if(aa=='gdp'){
+            const sData = db.GDP
+            let perPage = 10; 
+            let page = parseInt(req.params) || 1; 
+            var start =(page-1)*perPage
+            var end = page *perPage
+            res.send(sData.slice(start,end)) 
+        }
+        if(aa=='cpi'){
+            const sData = db.CPI
+            let perPage = 10; 
+            let page = parseInt(req.params) || 1; 
+            var start =(page-1)*perPage
+            var end = page *perPage
+            res.send(sData.slice(start,end)) 
+        }
     }
 
 }
--- api/routes.js
+++ api/routes.js
@@ -9,13 +9,15 @@ module.exports = function(app) {
 
 
   // GDP Routes
-  app.route('/gdp')
+  app.route('/macro')
     .get(gdpCtrl.get);
-  app.route('/gdp/:page')
-    .get(gdpCtrl.paging)
-  app.route('/gdpsearch')
+  app.route('/macro/:type')
+    .get(gdpCtrl.detail);
+  app.route('/macro/:type/search')
     .get(gdpCtrl.search)
-  app.route('/gdp/:dataId')
+  app.route('/macro/:type/:page')
+    .get(gdpCtrl.paging)
+  app.route('/macro/:type/:dataId')
     .post(gdpCtrl.store)
     .delete(gdpCtrl.delete);
     

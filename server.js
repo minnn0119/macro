25238db7b73a38ac2f05b60dc432e0ee270908c2
minnn0119 <83383771+minnn0119@users.noreply.github.com>
2022-11-06 15:02:59 +0700

    Update server.js

--- server.js
+++ server.js
@@ -27,13 +27,13 @@ routes(app)
 
 app.use(cors(corsOpts));
 
-// app.use(function (req, res, next) {
-//     res.setHeader('Access-Control-Allow-Origin', '*');
-//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-//     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-//     res.setHeader('Access-Control-Allow-Credentials', true);
-//     next();
-//     })
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+    })
 app.use(function(req, res) {
     res.status(404).send({url: req.originalUrl + ' not found'})
 })
